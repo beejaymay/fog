134817a2d6ba189b08d2ce723cfbe619619b6338
B.J. May <bernard.may.hea4@statefarm.com>
2016-12-21 12:22:22 -0600

    Twitter now suppresses only self

--- facebook.js
+++
@@ -1,7 +0,0 @@
-var loc = window.location.pathname.split('/')[1];
-chrome.storage.sync.get(function(items) {
-  if (items.facebook !== loc) {
-    var styleHTML = '<link rel="stylesheet" type="text/css" href="' + chrome.runtime.getURL("facebook_show.css") + '">'
-    document.head.insertAdjacentHTML('beforeend', styleHTML);
-  }
-});
--- twitter.js
+++ twitter.js
@@ -1,6 +1,14 @@
-chrome.storage.sync.get(function(items) {
-  if (false) { //make this clever
-    var styleHTML = '<link rel="stylesheet" type="text/css" href="' + chrome.runtime.getURL("twitter_show.css") + '">'
-    document.head.insertAdjacentHTML('beforeend', styleHTML);
-  }
-});
+var userName = document.querySelector("[data-nav='view_profile']").pathname.substring(1);
+
+var customStyles = [
+  '[data-element-term="follower_stats"] > .ProfileCardStats-statValue { visibility: visible !important; }',
+  '.ProfileCardStats-statLink[href="/' + userName + '/followers"] > .ProfileCardStats-statValue { visibility: hidden !important; }',
+  '[data-nav="followers"] > .ProfileNav-value { visibility: visible !important; }',
+  '[data-nav="followers"][href="/followers"] > .ProfileNav-value { visibility: visible !important; }'
+].join('\n');
+
+var styles = document.createElement('style');
+styles.appendChild(document.createTextNode(customStyles));
+
+document.head.appendChild(styles);
+console.log(userName);
