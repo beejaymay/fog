64a154901914e95aa5082c71744306b6d562d123
B.J. May <beejay.may@gmail.com>
2016-12-21 21:03:48 -0600

    twitter fixes

--- twitter.js
+++ twitter.js
@@ -1,10 +1,10 @@
-var userName = document.querySelector("[data-nav='view_profile']").pathname.substring(1);
+var userName = JSON.parse(document.querySelector("#init-data").value).screenName;
 
 var customStyles = [
   '[data-element-term="follower_stats"] > .ProfileCardStats-statValue { visibility: visible !important; }',
-  '.ProfileCardStats-statLink[href="/' + userName + '/followers"] > .ProfileCardStats-statValue { visibility: hidden !important; }',
   '[data-nav="followers"] > .ProfileNav-value { visibility: visible !important; }',
-  '[data-nav="followers"][href="/followers"] > .ProfileNav-value { visibility: visible !important; }'
+  '.ProfileCardStats-statLink[href="/' + userName + '/followers"] > .ProfileCardStats-statValue { visibility: hidden !important; }',
+  '[data-nav="followers"][href="/followers"] > .ProfileNav-value { visibility: hidden !important; }'
 ].join('\n');
 
 var styles = document.createElement('style');
